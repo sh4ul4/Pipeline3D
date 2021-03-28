4944db4534f3167c5b1c0fc857ea738f0feeb420
Vince780 <vincent_xavier780@yahoo.fr>
2021-03-28 12:58:47 +0200

    Modification de Sommet.hpp

--- specs/Sommet.hpp
+++ specs/Sommet.hpp
@@ -12,9 +12,12 @@ class Sommet
 {
 public:
 	//Coordonnées flotantes x,y,z
-	float x;
-	float y;
-	float z;
+	float x, y, z;
+
+	/**********************************************************************************************
+	 * 		Constructeurs
+	 * *******************************************************************************************/
+
 	// Constructeur par défaut
 	Sommet() : x(0), y(0), z(0) {}
 
@@ -33,103 +36,82 @@ public:
 
 	//Constructeur par copie
 	Sommet(const Sommet &v) : x(v.x), y(v.y), z(v.z) {}
-	
-	/* --- Surchages d'opérateurs --- */
-	Sommet &operator=(const Sommet &other); // Le sommet courant prend les coordonnées du sommet en paramètre
-	Sommet &operator+=(const Sommet &other); // Les coordonnées du sommet courant sont égal à la somme d'eux-même avec les coordonnées du sommet en paramètre 
-	Sommet &operator-=(const Sommet &other); // ---- différence ---
-	
-	Sommet &operator*=(const float &val); // Multiplication des coordonnées du sommet courant par une valeur float
-	Sommet &operator*=(const int &val); // Multiplication des coordonnées du sommet courant par une valeur int
-	Sommet &operator*=(const double &val); // Multiplication des coordonnées du sommet courant par une valeur double
-	Sommet &operator/=(const float &val); // Division des coordonnées du sommet courant par une valeur float
-	Sommet &operator/=(const int &val); // Division des coordonnées du sommet courant par une valeur int
-	Sommet &operator/=(const double &val); // Division des coordonnées du sommet courant par une valeur double
 
-	const Sommet operator*(const float &val) const
-	{
-		return Sommet(x * val, y * val, z * val);
-	}
-	const Sommet operator*(const double &val) const
-	{
-		return Sommet(x * val, y * val, z * val);
-	}
-	const Sommet operator*(const int &val) const
-	{
-		return Sommet(x * val, y * val, z * val);
-	}
-	const Sommet operator/(const float &val) const
-	{
-		//if(val == 0) { std::cout << "ERROR : cannot divide by 0" << std::endl; exit(1); }
-		return Sommet(x / val, y / val, z / val);
-	}
-	const Sommet operator/(const double &val) const
-	{
-		//if(val == 0) { std::cout << "ERROR : cannot divide by 0" << std::endl; exit(1); }
-		return Sommet(x / val, y / val, z / val);
-	}
-	const Sommet operator/(const int &val) const
-	{
-		//if(val == 0) { std::cout << "ERROR : cannot divide by 0" << std::endl; exit(1); }
-		return Sommet(x / val, y / val, z / val);
-	}
-	float &operator[](const unsigned &i)
-	{
-		switch (i)
-		{
-		case 0:
-			return x;
-		case 1:
-			return y;
-		case 2:
-			return z;
-		}
-		FATAL_ERR("Index out of bounds for instance of [Vertex]")
-	}
-	// Return Point3D with the value of sommet
-	Point3D toPoint3D() const
-	{
-		return Point3D(x, y, z);
-	}
+	/**********************************************************************************************
+	 * 		Surcharges d'opérateur
+	 * *******************************************************************************************/
+
+	// Le sommet courant prend les coordonnées du sommet en paramètre
+	Sommet &operator=(const Sommet &other);
+	// Les coordonnées du sommet courant sont égal à la somme d'eux-même avec les coordonnées du sommet en paramètre
+	Sommet &operator+=(const Sommet &other);
+	// Les coordonnées du sommet courant sont égal à la différence entre les coordonnées du sommet courant et ceux du sommet en paramètre
+	Sommet &operator-=(const Sommet &other);
+
+	// Multiplication des coordonnées du sommet courant par une valeur float
+	Sommet &operator*=(const float &val);
+	// Multiplication des coordonnées du sommet courant par une valeur int
+	Sommet &operator*=(const int &val);
+	// Multiplication des coordonnées du sommet courant par une valeur double
+	Sommet &operator*=(const double &val);
+	// Division des coordonnées du sommet courant par une valeur float
+	Sommet &operator/=(const float &val);
+	// Division des coordonnées du sommet courant par une valeur int
+	Sommet &operator/=(const int &val);
+	// Division des coordonnées du sommet courant par une valeur double
+	Sommet &operator/=(const double &val);
+
+	// Multiplie les coordonnées du sommet courant par une valeur float et renvoie le nouveau sommet
+	const Sommet operator*(const float &val) const;
+	// Multiplie les coordonnées du sommet courant par une valeur double et renvoie le nouveau sommet
+	const Sommet operator*(const double &val) const;
+	// Multiplie les coordonnées du sommet courant par une valeur int et renvoie le nouveau sommet
+	const Sommet operator*(const int &val) const;
+	// Divise les coordonnées du sommet courant par une valeur float et renvoie le nouveau sommet
+	const Sommet operator/(const float &val) const;
+	// Divise les coordonnées du sommet courant par une valeur double et renvoie le nouveau sommet
+	const Sommet operator/(const double &val) const;
+	// Divise les coordonnées du sommet courant par une valeur int et renvoie le nouveau sommet
+	const Sommet operator/(const int &val) const;
+
+	// Opérateur permettant de récupérer la valeur d'une coordonnée
+	float &operator[](const unsigned &i);
+
+	/**********************************************************************************************
+	 * 		Méthodes de classe
+	 * *******************************************************************************************/
+
+	// Convertie un sommet en un point 3D
+	Point3D toPoint3D() const;
 	//Le sommet devient à une distance de l'origine dans la même direction.
 	void normalize()
 	{
 		const float length = getLength();
 		*this /= length;
 	}
-	//Renvoie le produit scalaire
-	float dotProduct(const Sommet &v) const
-	{
-		return x * v.x + y * v.y + z * v.z;
-	}
-	//Renvoie un sommet qui le résultat produit en croix de 2 sommets
-	Sommet crossProduct(const Sommet &v) const
-	{
-		return Sommet(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
-	}
-	//Calcul de distance entre 2 sommets
-	float distance(const Sommet &v) const
-	{
-		return sqrtf((x - v.x) * (x - v.x) + (y - v.y) * (y - v.y) + (z - v.z) * (z - v.z));
-	}
+	// Renvoie le produit scalaire effectué entre le les coordonnées du sommet courant et le sommet en argument
+	float dotProduct(const Sommet &v) const;
+	// Renvoie un sommet qui est le résultat du produit en croix du sommet courant avec le sommet en argument
+	Sommet crossProduct(const Sommet &v) const;
+	// Calcul de distance entre le sommet courant et le sommet en argument
+	float distance(const Sommet &v) const;
 
 	/**
 	 * @brief Calcule la distance le point actuel et 2 points formant une ligne
 	 * 
 	 * @param Sommet& P0 Premier point formant la ligne
-	 * @param Sommet& P2 Deuxième point formant la ligne
+	 * @param Sommet& P1 Deuxième point formant la ligne
 	 * 
 	 * @return Distance flottante entre le point actuel et la ligne donnée
 	 */
 	float distanceToLine(const Sommet &P0, const Sommet &P1) const;
 
-	//Afficher les coordonnées du sommet par stdout
-	void print(void) const
-	{
-		std::cout << "[" << x << "|" << y << "|" << z << "]";
-	}
+	// Afficher les coordonnées du sommet par stdout
+	void print(void) const;
+	// Affiche les coordonées des sommets en surchageant l'opérateur <<
 	friend std::ostream &operator<<(std::ostream &os, const Sommet &v);
 
+	// ???????????????????
 	void normalizeOnLength(const float &length)
 	{
 		const float sum = abs(x) + abs(y) + abs(z);
@@ -143,46 +125,7 @@ public:
 		y *= normalizedLength;
 		z *= normalizedLength;
 	}
-	// Retourne la distance par rapport à l'origine du rapport
-	float getLength(void) const
-	{
-		return sqrtf(x * x + y * y + z * z);
-	}
+
+	// Retourne la distance par rapport à l'origine du repère
+	float getLength() const;
 };
-//Surchage d'opérateur
-inline const Sommet operator+(const Sommet &v1, const Sommet &v2)
-{
-	return Sommet(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-}
-inline const Sommet operator-(const Sommet &v1, const Sommet &v2)
-{
-	return Sommet(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-}
-inline const Sommet operator*(const Sommet &v1, const Sommet &v2)
-{
-	return Sommet(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
-}
-inline const Sommet operator/(const Sommet &v1, const Sommet &v2)
-{
-	return Sommet(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
-}
-
-float Sommet::distanceToLine(const Sommet &P0, const Sommet &P1) const
-{
-	Sommet v = P1 - P0;
-	const Sommet w = *this - P0;
-
-	const float c1 = w.dotProduct(v);
-	if (c1 <= 0) // before P0
-		return this->distance(P0);
-	const float c2 = v.dotProduct(v);
-	if (c2 <= c1) // after P1
-		return this->distance(P1);
-	v *= (c1 / c2);
-	return this->distance(P0 + v);
-}
-//affiche les coordonées des sommets en surchageant l'opérateur <<
-std::ostream &operator<<(std::ostream &os, const Sommet &v)
-{
-	return (os << "[" << v.x << "|" << v.y << "|" << v.z << "]");
-}
