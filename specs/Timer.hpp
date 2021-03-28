dab3f85398b6c082f739fb5f4dead8b395aa9de1
Vince780 <vincent_xavier780@yahoo.fr>
2021-03-28 14:24:19 +0200

    Modification mineure de Timer.hpp

--- specs/Timer.hpp
+++ specs/Timer.hpp
@@ -1,16 +1,17 @@
 #pragma once
 
 /**
- * @brief Minuteur
- * Crée un Minuteur qui sur demande renvoie si le temps demandé est écoulé ou non
+ * @class Chrono
+ * @brief Crée un Minuteur qui sur demande renvoie si le temps demandé est écoulé ou non
  */
 class Chrono {
 private:
 	Uint32 start = NULL;
 	Uint32 stop = NULL;
 public:
-	Uint32 setEndChrono(const Uint32& end){ // Définit l'heure d'arrêt du minuteur
-	}
-	bool isTimerEnded(void) { // Vérifie si le minuteur est fini
-	}
+	// Définit l'heure d'arrêt du minuteur
+	Uint32 setEndChrono(const Uint32& end);
+	
+	// Vérifie si le minuteur est fini
+	bool isTimerEnded();
 };
