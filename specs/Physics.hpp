27c1c8530079057d218ede4beaf5cb584d4c80c7
Vince780 <vincent_xavier780@yahoo.fr>
2021-04-14 22:10:35 +0200

    namespace Cinematic dans Physics

--- specs/Physics.hpp
+++ specs/Physics.hpp
@@ -1,5 +1,5 @@
 
-// Région déclarative contenant des valeurs et des méthodes concernant la physique étudiant le mouvement 
+// Région déclarative contenant des valeurs et des méthodes concernant la physique étudiant le mouvement
 namespace Physics
 {
 	Keyboard keyboard;
@@ -18,14 +18,30 @@ namespace Physics
 		float gravity = 9.8f;
 		bool applyGravity = false;
 		int timeFalling = 0;
-		float getGravityForce()
-		{
-			if (applyGravity == false)
-				return 0;
-			//timeFalling++;
-			//return gravity / 50;
-			return timeFalling * gravity / 1000;
-		}
+		float getGravityForce();
+	}
+
+	namespace Cinematic
+	{
+		/* ----- FORMULES PHYSIQUES ----- */
+
+		/**
+		 * @brief Calcul la force d'attraction entre deux corps
+		 * 
+		 * @param l1 Premier corp
+		 * @param l2 Deuxième corp
+		 * @return  
+		 */
+		float attractionForce(Luminary *l1, luminary *l2);
+
+		/**
+		 * @brief Calcul l'accélération d'un astre en fonction des forces exercées sur celui-ci
+		 * 
+		 * @param mass Masse de la planète
+		 * @param force Forces exercées sur la planète (il faudrait un std::vector des forces)
+		 * @return std::pair<float, float> --> vecteur accélération
+		 */
+		std::pair<float, float> Newton2nd(float mass, std::pair<float, float> force);
 	}
 
 	/* ----- Méthodes ----- */
