b8246d2291826c33a39ffedbb41713f01628d0d7
Louis Raban-Schürmann <louis-blau@orange.fr>
2021-04-19 19:06:41 +0200

    handling asserts

--- headers/ErrorsHandler.hpp
+++ headers/ErrorsHandler.hpp
@@ -7,7 +7,13 @@
 #define FATAL_ERR(s) { std::cout << "FATAL -- " << s << std::endl; exit(1); }
 
 /**
- * Erreur 
+ * @brief Erreur
  * Affiche un message d'erreur sans quitter le programme
  */ 
 #define PRINT_ON_ERR(s) { std::cout << "ERR -- " << s << std::endl; }
+
+ /**
+  * @brief Assert
+  * Si b est évalué à false, affiche un message d'erreur et met fin au programme
+  */
+#define ASSERT(b, s) { if(!b) std::cout << "FATAL -- " << s << std::endl; exit(1); }
