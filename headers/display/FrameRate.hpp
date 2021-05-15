b30e036831510f6295cfc6d5f8124659b9409f21
fm16191 <fm16191>
2021-05-15 16:36:42 +0200

    limitation framerate

--- headers/display/FrameRate.hpp
+++ headers/display/FrameRate.hpp
@@ -1,75 +1,98 @@
 #pragma once
 
-class StableFramerate {
+class StableFramerate
+{
 private:
-
 	/**
 	 * Une variable privée.
 	 * Compte le nombre de frames crées depuis le début
 	 */
 	Uint32 totalFrames = 0;
-	
+
 	/**
 	 * Une variable privée.
 	 * Temps du prochain rendu de frame
 	 */
-	Uint32 nextTime = 0; 
-	
+	Uint32 nextTime = 0;
+
 	/**
 	 * Une variable privée.
 	 * Chronometre pour attendre 1000 ticks
 	 */
-	Timer timer; 
-	
+	Timer timer;
+
 	/**
 	 * Une variable privée.
 	 * Compte le nombre de frames pour le framerate
 	 */
-	short int frameCounter = 0; 
-	
+	short int frameCounter = 0;
+
 	/**
 	 * Une variable privée.
 	 * Framerate actuel 
 	 */
 	short int framerate = 0;
-	
+
 	/**
 	 * Une variable privée.
 	 * Temps actuel en SDL_GetTicks()
 	 */
-	Uint32 now = 0; 
-public:
+	Uint32 now = 0;
 
-	StableFramerate():timer(1000) {}
+	/**
+	 * Une variable privée.
+	 * Temps écoulé en ms
+	 */
+	float elapsedMS = 0;
+
+public:
+	StableFramerate() : timer(1000) {}
 
 	/**
 	 * Une fonction publique.
 	 * @brief Limite les calculs et le rendering
 	 * @param framerate Limite imposée
 	 */
-	void stabilizeCalculationAndRendering(const double& framerate = 0) {
-		if (framerate == 60) nextTime += 17;
-		else if (framerate == 25)nextTime += 43.65954;
-		else { nextTime += 1.621059 + 181237898.4 / (1 + pow(framerate / 0.0000460458, 1.156912)); }
-		now = clock(); // current time in SDL_Ticks
-		if (nextTime > now) {
-			SDL_Delay(Uint32(nextTime - now)); // time to wait before rendering next frame
+	void stabilizeCalculationAndRendering(const double &framerate_max = 0, Uint64 *start = 0)
+	{
+		Uint32 ticksPerFrame = 1000.0f / float(framerate_max);
+		Uint64 end = SDL_GetPerformanceCounter();
+		elapsedMS = (end - *start) / (float)SDL_GetPerformanceFrequency() * 1000.0f;
+
+		// Cap to 60 FPS
+		Uint32 waitingTime = floor(ticksPerFrame - elapsedMS);
+		// std::cout << elapsedMS << "_" << waitingTime << std::endl;
+		// std::cout << "Current FPS: " << floor(1000.0f / elapsedMS) << std::endl;
+		if (elapsedMS < ticksPerFrame)
+		{
+			SDL_Delay(waitingTime);
 		}
 	}
-	
+
 	/**
 	 * Une fonction publique.
 	 * @brief Limite le rendering pour l'adapter a 60 framerates dans la fenetre ( window)
 	 * @param framerate Limite imposée
 	 * @param window Ecran d'affichage
 	 */
-	void stabilizeRendering(Window* window, double framerate = 0) {
+	void stabilizeRendering(Window *window, double framerate = 0)
+	{
 		// optimized formula with approximative constants
-		if (framerate == 60) { framerate = 17; }
-		else { framerate = 1.621059 + (181237898 / (1 + pow(framerate / 0.0000460458, 1.156912))); }
+		if (framerate == 60)
+		{
+			framerate = 17;
+		}
+		else
+		{
+			framerate = 1.621059 + (181237898 / (1 + pow(framerate / 0.0000460458, 1.156912)));
+		}
 		now = SDL_GetTicks(); // current time in SDL_Ticks
-		if (nextTime == 0) { nextTime = now + framerate; }
-		if (nextTime < now) {
+		if (nextTime == 0)
+		{
+			nextTime = now + framerate;
+		}
+		if (nextTime < now)
+		{
 			window->RenderScreen();
 			nextTime = 0;
 		}
@@ -81,21 +104,31 @@ public:
 	 * @param x,y Coordonnées du point où sera affiché le framerate
 	 * @param renderer Moteur de rendu SDL
 	 */
-	void renderFrameRate(const int& X, const int& Y, SDL_Renderer* renderer) {
+	void renderFrameRate(const int &X, const int &Y, SDL_Renderer *renderer)
+	{
 		frameCounter++;
-		if (timer.set(1 * CLOCKS_PER_SEC)) {
+		// std::cout << "framecounter: " << frameCounter << std::endl;
+		// std::cout << "Current FPS: " << floor(1000.0f / elapsedMS) << std::endl;
+
+		// if (timer.set(1 * CLOCKS_PER_SEC)) {
+		if (time(0) > now)
+		{							  // Si une seconde en utime s'est écoulée
 			framerate = frameCounter; // return the framerate after 1 sec
 			frameCounter = 0;
 		}
-		Draw::RenderDigits(X, Y, 8, framerate, red, renderer);
+		now = time(0);
+
+		// Draw::RenderDigits(X, Y, 8, framerate, red, renderer);
+		Draw::RenderDigits(X, Y, 8, floor(framerate), red, renderer);
 	}
-	
+
 	/**
 	 * Une fonction publique.
 	 * @brief Creer un nouveau frame et en augmente le compteur
 	 * @param void
 	 */
-	void newFrame(void) {
+	void newFrame(void)
+	{
 		frameCounter++;
 	}
 };
--- headers/display/Render.hpp
+++ headers/display/Render.hpp
@@ -15,7 +15,7 @@ class Render
 	StableFramerate framerate;
 
 	// Prochain triangle à render
-	std::vector<Triangle*> toRender;
+	std::vector<Triangle *> toRender;
 
 	// nombre de shapes incluses après la dernière mise-à-jour de toRender
 	size_t shapesNumber = 0;
@@ -24,15 +24,16 @@ public:
 	// Texture globale
 	GlobalTexture globalTexture;
 
-	void savePNG(const std::string& file) {
+	void savePNG(const std::string &file)
+	{
 		globalTexture.savePNG(file);
 	}
-
 	// Interdiction d'utilisation d'un constructeur vide
 	Render() = delete;
 
 	// Constructeur de la classe Render, appelant le constructeur de globalTexture
-	Render(const Window& window, int w, int h) :globalTexture(window, w, h) {
+	Render(const Window &window, int w, int h) : globalTexture(window, w, h)
+	{
 		SDL_GL_SetSwapInterval(0);
 		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
 		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
@@ -42,24 +43,27 @@ public:
 		TTF_Init();
 	}
 
-	// Change la taille d'un triangle pour que ça rentre 
-	void updateTriangles(ShapeManager& manager) {
+	// Change la taille d'un triangle pour que ça rentre
+	void updateTriangles(ShapeManager &manager)
+	{
 		const size_t size = toRender.size();
-		
+
 		// Quitter la fonction s'il n'y a pas eu de changement depuis la précédente update
-		if (manager.rendered) 	return;
-		
+		if (manager.rendered)
+			return;
+
 		toRender.clear();
 		toRender.shrink_to_fit();
 		toRender.reserve(size);
 
-		for (size_t j = 0; j < manager.shapes.size(); j++) {
+		for (size_t j = 0; j < manager.shapes.size(); j++)
+		{
 			// Ne pas render les shapes marquées non visibles
-			if (!manager.shapes[j]->visible)  
+			if (!manager.shapes[j]->visible)
 				continue;
 
 			const size_t max2 = manager.shapes[j]->triangles.size();
-			for (size_t i = 0; i < max2; i++) {
+			for (size_t i = 0; i < max2; i++){
 				toRender.push_back(&manager.shapes[j]->triangles[i]);
 			}
 		}
@@ -69,14 +73,17 @@ public:
 
 private:
 	// Rendering des triangles
-	void renderTriangles(const Window& window) {
+	void renderTriangles(const Window &window)
+	{
 		// render textures on surface
 		// signlethreaded version
 		const size_t size = toRender.size();
 		const Point2D<int> center(globalTexture.getWidth() / 2, globalTexture.getHeight() / 2);
-		for (size_t i = 0; i < size; i++) {
+		for (size_t i = 0; i < size; i++)
+		{
 			toRender[i]->visible = false;
-			if (toRender[i]->normalVec.dot(Camera::getCurrent().look) > 0.2)continue;
+			if (toRender[i]->normalVec.dot(Camera::getCurrent().look) > 0.2)
+				continue;
 			toRender[i]->setScreenCoord(window, center, Camera::getCurrent());
 			// render triangle
 			toRender[i]->render(window, globalTexture, center);
@@ -84,31 +91,45 @@ private:
 	}
 
 public:
-	void renderStatic(const Point2D<int>& topLeft, const int& width, const int& height, const Window& window) {
+	void renderStatic(const Point2D<int> &topLeft, const int &width, const int &height, const Window &window)
+	{
 		SDL_ShowCursor(true);
 		globalTexture.renderTexture(window.getRenderer(), topLeft, width, height, 0, 0);
 	}
 	// Rendering écran
-	void render(const Point2D<int>& topLeft, const int& width, const int& height, InputEvent& inputEvent, const Window& window, ShapeManager& manager) {
-		if (Camera::currentExists()) {
+	void render(const Point2D<int> &topLeft, const int &width, const int &height, InputEvent &inputEvent, const Window &window, ShapeManager &manager)
+	{
+		if (Camera::currentExists())
+		{
+
+			// For framerate control
+			Uint64 start = SDL_GetPerformanceCounter();
+
 			Physics::move(inputEvent, manager);
 			Camera::getCurrent().update(inputEvent, window, globalTexture, topLeft);
-			if (Camera::currentExists() == false) { std::cout << "Error : current camera does not exist.\n"; exit(1); }
+			if (Camera::currentExists() == false)
+			{
+				std::cout << "Error : current camera does not exist.\n";
+				exit(1);
+			}
 			globalTexture.refreshZbuffer();
 			globalTexture.clearPixels();
 			renderTriangles(window);
 			//globalTexture.filterBnW();
 			globalTexture.updateTexture();
 			globalTexture.renderTexture(window.getRenderer(), topLeft, width, height, 0, 0);
+			// framerate.setNextTime(now);
+			// }
+			framerate.stabilizeCalculationAndRendering(60, &start);
 		}
 
-		//framerate.stabilizeCalculationAndRendering(60);
 		//Wait(10);
 
 		framerate.renderFrameRate(10, 10, window.getRenderer());
 	}
 
-	void renderOrientation(const Point2D<int>& pos, float size, const Window& window)const {
+	void renderOrientation(const Point2D<int> &pos, float size, const Window &window) const
+	{
 		const Vertex camera(0, 0, 0);
 		Matrix<4, 4> viewMatrix = Camera::makeFPviewMatrix(camera, Camera::getCurrent().angleY, Camera::getCurrent().angleX);
 		const Vertex x(size / 2 - 2, 0, 0);
@@ -116,14 +137,14 @@ public:
 		const Vertex z(0, 0, size / 2 - 2);
 		const Vertex origin(0, 0, 0);
 		Matrix<4, 4> mo;
-		mo.m[0] = { origin.x,origin.y,origin.z,1 };
+		mo.m[0] = {origin.x, origin.y, origin.z, 1};
 		Matrix<4, 4> mx;
-		mx.m[0] = { x.x,x.y,x.z,1 };
+		mx.m[0] = {x.x, x.y, x.z, 1};
 		Matrix<4, 4> my;
-		my.m[0] = { y.x,y.y,y.z,1 };
+		my.m[0] = {y.x, y.y, y.z, 1};
 		Matrix<4, 4> mz;
-		mz.m[0] = { z.x,z.y,z.z,1 };
-		bool clip{ false };
+		mz.m[0] = {z.x, z.y, z.z, 1};
+		bool clip{false};
 		const Point2D<int> center(0, 0);
 		const Vertex oScreen = Camera::getCurrent().get2DWithoutPerspective(mo, clip, center, viewMatrix);
 		const Vertex xScreen = Camera::getCurrent().get2DWithoutPerspective(mx, clip, center, viewMatrix);
